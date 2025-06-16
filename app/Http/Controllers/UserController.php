3e14ad698a2011793f8aa631072f0044abb21846
joaovitorcruzz <joaovitordasilvacruz123@gmail.com>
2025-06-15 21:27:54 -0300

    feat: user controller to management all routes of api

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -11,20 +11,24 @@ use Illuminate\Validation\Rule;
 
 class UserController extends Controller
 {
+    // Amnésia na hora de desenvolver, esquecimento dos commits granulares, vou comentando inicio e fim
     /**
      * Retorna os dados do usuário autenticado.
      * Acessível por: user e admin.
      */
+    // Inicio da busca do profile
     public function profile()
     {
         $user = Auth::user();
         return response()->json($user);
     }
+    // Final da busca do profile
 
     /**
      * Atualiza os dados do próprio usuário autenticado.
      * Acessível por: user e admin.
      */
+    // Inicio do update do profile
     public function updateProfile(Request $request)
     {
         $user = Auth::user();
@@ -52,22 +56,26 @@ class UserController extends Controller
             'user' => $user
         ]);
     }
+    // Final do update do profile
 
     /**
      * Lista todos os usuários.
      * Acessível por: admin.
      */
+    // Inicio do get do admin
     public function index()
     {
         $users = User::all();
         return response()->json($users);
     }
+    // Final do get do admin
 
     /**
      * Atualiza os dados de um usuário específico.
      * Acessível por: admin.
      * Usamos o Route-Model Binding (User $user) para injetar o usuário automaticamente.
      */
+    // Incio do update do admin
     public function update(Request $request, User $user)
     {
         $validator = Validator::make($request->all(), [
@@ -94,11 +102,13 @@ class UserController extends Controller
             'user' => $user
         ]);
     }
+    // Final do update do admin
 
     /**
      * Deleta um usuário específico.
      * Acessível por: admin.
      */
+    //Inicio do delete do admin
     public function destroy(User $user)
     {
         if (Auth::id() === $user->id) {
@@ -109,4 +119,5 @@ class UserController extends Controller
 
         return response()->json(['message' => 'Usuário deletado com sucesso!'], 200);
     }
+    // Final do delete do admin
 }
