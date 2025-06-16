97dbf85f6f3e33d80f123f0343dc8f2d70defe63
joaovitorcruzz <joaovitordasilvacruz123@gmail.com>
2025-06-15 21:07:07 -0300

    feat: adjust routes api including public authenticated and admin

--- routes/api.php
+++ routes/api.php
@@ -9,17 +9,23 @@ use Illuminate\Support\Facades\Route;
 //     return $request->user();
 // })->middleware('auth:sanctum');
 
-
+// Amnésia na hora de desenvolver, esquecimento dos commits granulares, vou comentando inicio e fim
+// Inicio das rotas publicas
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
+// Final das rotas publicas
 
 Route::middleware('auth:api')->group(function () {
+    //Inicio das rotas autenticadas para o usuario
     Route::get('/profile', [UserController::class, 'profile']);
     Route::put('/profile', [UserController::class, 'updateProfile']);
+    //Final das rotas autenticadas para o usuario
 
     Route::middleware('admin')->group(function () {
+        //Inicio das rotas autenticadas para o admin
         Route::get('/users', [UserController::class, 'index']);
         Route::put('/users/{user}', [UserController::class, 'update']);
         Route::delete('/users/{user}', [UserController::class, 'destroy']);
+        //Final das rotas autenticadas para o admin
     });
 });
