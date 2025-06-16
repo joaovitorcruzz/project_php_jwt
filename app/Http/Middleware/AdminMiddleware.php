7c69db4bbca09dbcb80febccc7cdb209443a814f
joaovitorcruzz <joaovitordasilvacruz123@gmail.com>
2025-06-15 21:08:26 -0300

    feat: check validation on middleware

--- app/Http/Middleware/AdminMiddleware.php
+++ app/Http/Middleware/AdminMiddleware.php
@@ -16,10 +16,12 @@ class AdminMiddleware
      */
     public function handle(Request $request, Closure $next): Response
     {
+        // Amnésia na hora de desenvolver, esquecimento dos commits granulares, vou comentando inicio e fim
+        // Inicio Implementando validação se permissão é igual a admin ou não
         if (Auth::check() && Auth::user()->role === 'admin') {
             return $next($request);
         }
-
+        // Final Implementando validação se permissão é igual a admin ou não
         return response()->json(['error' => 'Acesso não autorizado.'], 403);
     }
 }
